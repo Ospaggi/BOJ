adb150b1e5415cbdcf0da64c1d1f6c32eeb8d2bd
hwany8677 <19703339+hwany8677@users.noreply.github.com>
2024-04-09 23:15:00 +0900

    2530번 完

--- 2530.py
+++ 2530.py
@@ -4,6 +4,7 @@ b=int(b)
 c=int(c)
 d=int(input())
 totsec=3600*a+60*b+c+d
-if totsec>=86400: 
-    totsec-=86400
-print(f"{totsec//3600} {(totsec-(totsec//3600)*3600)//60} {( (totsec-(totsec//3600)*3600)//60)}")
+if totsec >= 86400: 
+    totsec-=86400*(totsec//86400)
+min=(totsec-(totsec//3600)*3600)//60 
+print(f"{totsec//3600} {min} {totsec-(totsec//3600)*3600-min*60}")
